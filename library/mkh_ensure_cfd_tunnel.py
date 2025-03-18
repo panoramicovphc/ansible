bfe668be0342e7a32717816c3d61c1b896d8ecf0
Maurício Fedatto <mfedatto@gmail.com>
2025-03-17 21:52:00 -0300

    fix: simplify ingress existence check in mkh_ensure_cfd_tunnel.py

--- library/mkh_ensure_cfd_tunnel.py
+++ library/mkh_ensure_cfd_tunnel.py
@@ -120,7 +120,7 @@ def main():
         write_log(log_file, module, "Checking if ingress exists...")
         if 'result' in tunnel_config and tunnel_config['result'] is not None and \
            'config' in tunnel_config['result'] and tunnel_config['result']['config'] is not None and \
-           'ingress' in tunnel_config['result']['config'] and tunnel_config['result']['config']['ingress'] is not None:
+           'ingress' in tunnel_config['result']['config']:
             ingress_exists = next((item for item in tunnel_config['result']['config']['ingress'] if item['service'] == private_service and item['hostname'] == public_hostname), None)
             write_log(log_file, module, f"Ingress exists: {str(ingress_exists)}")
         else:
